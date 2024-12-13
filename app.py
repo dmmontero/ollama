d00f92f9b579b22df64202154cd28f408635d28a
Danny Mauricio Montero Valencia <dmontero@dnp.gov.co>
2024-12-13 17:22:42 -0500

    Menejo de excepciones

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # imports
 import streamlit as st
 from ollama import chat
+import httpx
 
 
 # Función para obtener respuesta de ollama.chat
@@ -25,18 +26,27 @@ st.write(" :outbox_tray: Presione enviar para recibir su respuesta")
 
 # Accion a ejecuar cuando se envia el formulario
 if submit:
-
-    with st.spinner(f"Procesando... :surfer: {pregunta}"):
-        respuesta = obtener_respuesta_ollama(pregunta)
-        # Mostrar la respuesta en un área de texto
-        # st.write(f"Pregunta:\n {pregunta} \n Respuesta:\n {respuesta}")
-        st.markdown(
-            f"""
-                :question: :blue-background[Pregunta]: {pregunta}
-                </br>
-                </br>
-                :speaking_head_in_silhouette: :green-background[Respuesta]: {respuesta}
-            """,
-            unsafe_allow_html=True,
+    if not pregunta:
+        st.error(":x: Campo obligatorio")
+        st.stop()
+    try:
+        with st.spinner(f"Procesando... :surfer: {pregunta}"):
+            respuesta = obtener_respuesta_ollama(pregunta)
+            # Mostrar la respuesta en un área de texto
+            # st.write(f"Pregunta:\n {pregunta} \n Respuesta:\n {respuesta}")
+            st.markdown(
+                f"""
+                    :question: :blue-background[Pregunta]: {pregunta}
+                    </br>
+                    </br>
+                    :speaking_head_in_silhouette: :green-background[Respuesta]: {respuesta}
+                """,
+                unsafe_allow_html=True,
+            )
+            st.success("Hecho!")
+    except httpx.ConnectError:
+        st.error(
+            ":no_entry_sign: Ocurrió un error de conexión, verifique que Ollama se este ejecutando"
         )
-        st.success("Hecho!")
+    except Exception as ex:
+        st.error(f":no_entry_sign: Ocurrió un error ejecutando la consulta {type(ex)}")
